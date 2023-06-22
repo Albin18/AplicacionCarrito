821998e3df1fd9a3f46c3ff913711542498def15
Albin18 <albianjose0424@gmail.com>
2023-06-22 00:17:50 -0400

    interface Front-End

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule, Routes} from "@angular/router";
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProductosComponent } from './productos/productos.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import{HttpClientModule} from '@angular/common/http';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'carrito', component: CarritoComponent},
+  {path: 'productos', component: ProductosComponent},
+  {path: 'home', component: HomeComponent}
+
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductosComponent,
+    CarritoComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/productos/productos.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import{Productos} from '../productos';
+import {HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+  private urlEndPoint: String = 'http://localhost:8080/'
+
+  constructor(private http: HttpClient){}
+
+  getProductos(): Observable<Productos[]> {
+    return this.http.get(this.urlEndPoint);
+  }
+
+}
